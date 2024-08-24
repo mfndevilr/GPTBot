17944ea072acde359d4302402bfac5541ed4a94f
mfndevilr <rasimtyy27@gmail.com>
2024-08-24 20:03:59 +0300

    version 0.1

---
+++ keyboard.py
@@ -0,0 +1,93 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+
+start_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='ChatGPTBot', callback_data='chatgpt'),
+         InlineKeyboardButton(text='Midjorni(не работает)', callback_data='images')]
+    ]
+)
+
+subscribe_menu = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton(text='Перейти в канал', url='https://t.me/prostochatgpt')]]
+)
+
+
+reply_menu = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Профиль💁'), KeyboardButton(text='Главная🏠')],
+    [KeyboardButton(text='Получить билеты🏦')]
+], resize_keyboard=True)
+
+tiket_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Покупка', callback_data='pay_tiket')],
+        [InlineKeyboardButton(text='Задания', callback_data='tasks_tiket')],
+        [InlineKeyboardButton(text='Промокод', callback_data='promocode_tiket')],
+        [InlineKeyboardButton(text='Пригласить друзей', callback_data='invite_tiket')]
+    ]
+)
+
+
+pay_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='10🎟',callback_data='pay_tiket_10')],
+        [InlineKeyboardButton(text='30🎟',callback_data='pay_tiket_30')],
+        [InlineKeyboardButton(text='50🎟',callback_data='pay_tiket_50')],
+        [InlineKeyboardButton(text='100🎟',callback_data='pay_tiket_100')],
+        [InlineKeyboardButton(text='150🎟',callback_data='pay_tiket_150')],
+        [InlineKeyboardButton(text='500🎟',callback_data='pay_tiket_500')],
+    ]
+)
+
+
+pay_tiket_10 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Оплатить 10🎟', callback_data='pay_tiket_10_pay')]
+    ]
+)
+
+pay_tiket_30 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Оплатить 30🎟', callback_data='pay_tiket_30_pay')]
+    ]
+)
+
+
+pay_tiket_50 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Оплатить 50🎟', callback_data='pay_tiket_50_pay')]
+    ]
+)
+pay_tiket_100 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Оплатить 100🎟', callback_data='pay_tiket_100_pay')]
+    ]
+)
+pay_tiket_150 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Оплатить 150🎟', callback_data='pay_tiket_150_pay')]
+    ]
+)
+pay_tiket_500 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Оплатить 500🎟', callback_data='pay_tiket_500_pay')]
+    ]
+)
+
+admin_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Добавть промокод', callback_data='add_promocode'),
+         InlineKeyboardButton(text='Удалить промокод', callback_data='delete_promocode')],
+        [InlineKeyboardButton(text='Вывести список промокодов', callback_data='get_promocodes')],
+        [InlineKeyboardButton(text='Добавить администратора', callback_data='add_admin'),
+         InlineKeyboardButton(text='Удалить администратора', callback_data='delete_admin')],
+        [InlineKeyboardButton(text='Вывести список администраторов', callback_data='get_admins')],
+        [InlineKeyboardButton(text='Рассылка', callback_data='send_notification'),
+         InlineKeyboardButton(text='Cписок пользователей', callback_data='get_users')]
+    ]
+)
+
+
+
+
+
+
---
+++ pay_hendler.py
@@ -0,0 +1,133 @@
+from aiogram.types import LabeledPrice, Message
+from aiogram import types, F, Router
+from main import bot
+import sqlite3 as sq3
+
+
+router = Router()
+
+@router.callback_query(F.data == 'pay_tiket_10')
+async def create_invoice_10(callback: types.CallbackQuery):
+    pay_10 = LabeledPrice(label='10🎟', amount=10)
+    await bot.send_invoice(
+        callback.message.chat.id,
+        title="Оплата",
+        description="купить 10🎟",
+        provider_token="",
+        currency="XTR",
+        is_flexible=False,
+        prices=[pay_10],
+        start_parameter="one-more",
+        payload="10"
+    )
+
+
+
+
+@router.callback_query(F.data == 'pay_tiket_30')
+async def create_invoice_30(callback: types.CallbackQuery):
+    pay_30 = LabeledPrice(label='30🎟', amount=30)
+
+    await bot.send_invoice(
+        callback.message.chat.id,
+        title="Оплата",
+        description="купить 30🎟",
+        provider_token="",
+        currency="XTR",
+        is_flexible=False,
+        prices=[pay_30],
+        start_parameter="one-more",
+        payload="30"
+    )
+
+@router.callback_query(F.data == 'pay_tiket_50')
+async def create_invoice_50(callback: types.CallbackQuery):
+    pay_50 = LabeledPrice(label='50🎟', amount=50)
+
+    await bot.send_invoice(
+        callback.message.chat.id,
+        title="Оплата",
+        description="купить 50🎟",
+        provider_token="",
+        currency="XTR",
+        is_flexible=False,
+        prices=[pay_50],
+        start_parameter="one-more",
+        payload="50"
+    )
+
+@router.callback_query(F.data == 'pay_tiket_100')
+async def create_invoice(callback: types.CallbackQuery):
+    pay_100 = LabeledPrice(label='100🎟', amount=100)
+
+    await bot.send_invoice(
+        callback.message.chat.id,
+        title="Оплата",
+        description="купить 100🎟",
+        provider_token="",
+        currency="XTR",
+        is_flexible=False,
+        prices=[pay_100],
+        start_parameter="one-more",
+        payload="100"
+    )
+
+@router.callback_query(F.data == 'pay_tiket_150')
+async def create_invoice(callback: types.CallbackQuery):
+    pay_150 = LabeledPrice(label='150🎟', amount=150)
+
+    await bot.send_invoice(
+        callback.message.chat.id,
+        title="Оплата",
+        description="купить 150🎟",
+        provider_token="",
+        currency="XTR",
+        is_flexible=False,
+        prices=[pay_150],
+        start_parameter="one-more",
+        payload="150"
+    )
+
+@router.callback_query(F.data == 'pay_tiket_500')
+async def create_invoice(callback: types.CallbackQuery):
+    pay_500 = LabeledPrice(label='500🎟', amount=500)
+
+    await bot.send_invoice(
+        callback.message.chat.id,
+        title="Оплата",
+        description="купить 500🎟",
+        provider_token="",
+        currency="XTR",
+        is_flexible=False,
+        prices=[pay_500],
+        start_parameter="one-more",
+        payload="500"
+    )
+
+
+@router.pre_checkout_query()
+async def checkout_handler(checkout_query: types.PreCheckoutQuery):
+    await checkout_query.answer(ok=True)
+
+
+@router.message(F.successful_payment)
+async def star_payment(message: Message):
+    # Извлекаем payload из успешного платежа
+    payload = message.successful_payment.invoice_payload
+
+    conn = sq3.connect('user_baze.db3')
+    cur = conn.cursor()
+    query = "SELECT * FROM employees WHERE id = ?"
+    cur.execute(query, (f'{message.from_user.id}',))
+    rows = cur.fetchall()
+    token_user = []
+    for row in rows:
+        print(row)
+        token_user.append(row[-1])
+    cur.execute(f"UPDATE employees SET token = '{token_user[0] + int(payload)}' WHERE id = '{message.from_user.id}'")
+    conn.commit()
+    cur.close()
+    conn.close()
+    await message.answer(text=f'Ваш баланс пополнился на {int(payload)}🎟')
+
+
